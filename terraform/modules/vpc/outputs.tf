9de8a2630df60c3dd9a168bb09eb63b32af3823e
Kapildev <kdsinghse20@gmail.com>
2026-06-19 03:10:51 +0000

    Added terraform for VPC and Instance

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_app_subnet_id" {
+  value = aws_subnet.private_app.id
+}
+
+output "private_ai_subnet_id" {
+  value = aws_subnet.private_ai.id
+}
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "private_app_subnet_cidr" {
+  type = string
+}
+
+variable "private_ai_subnet_cidr" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
