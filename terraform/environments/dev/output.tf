9a30df116496c412fc9aee3601de7f1d6a220a54
Kapildev Singh <kapil@vozzo.ai>
2026-07-27 21:06:11 +0530

    setup ansible configuration file

--- terraform/environments/dev/output.tf
+++ terraform/environments/dev/output.tf
@@ -2,9 +2,9 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 output "openwebui_private_ip" {
-  value = module.ec2.private_ip
+  value = module.openwebui.private_ip
 }
 
 output "ollama_private_ip" {
-  value = module.ec2.private_ip
+  value = module.ollama.private_ip
 }
