4ee696006ebb23e2b7e5c196b9c48c36998a28b0
Kapildev Singh <kapil@vozzo.ai>
2026-07-27 21:06:52 +0530

    setup ansible configuration file

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -6,6 +6,14 @@ output "private_ip" {
   value = aws_instance.this.private_ip
 }
 
-output "public_ip" {
-  value = aws_instance.this.public_ip
+output "private_dns" {
+  value = aws_instance.this.private_dns
+}
+
+output "availability_zone" {
+  value = aws_instance.this.availability_zone
+}
+
+output "security_group_ids" {
+  value = aws_instance.this.vpc_security_group_ids
 }
