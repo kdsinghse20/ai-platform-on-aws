fe3fa8fb490ccc9e168ad13e89b1855afe9c4efa
Kapildev Singh <kapil@vozzo.ai>
2026-07-27 22:58:33 +0530

    update dynamic inventory as instance id for ansible

--- terraform/environments/dev/ansible_inventory.tf
+++ terraform/environments/dev/ansible_inventory.tf
@@ -3,7 +3,7 @@ resource "local_file" "ansible_inventory" {
   filename = "../../../ansible/inventory/hosts.ini"
 
   content = templatefile("${path.module}/inventory.tpl", {
-    ollama_private_ip    = module.ollama.private_ip
-    openwebui_private_ip = module.openwebui.private_ip
+    ollama_private_ip    = module.ollama.instance_id
+    openwebui_private_ip = module.openwebui.instance_id
   })
 }
--- terraform/environments/dev/output.tf
+++ terraform/environments/dev/output.tf
@@ -8,3 +8,11 @@ output "openwebui_private_ip" {
 output "ollama_private_ip" {
   value = module.ollama.private_ip
 }
+
+output "ollama_instance_id" {
+  value = module.ollama.instance_id
+}
+
+output "openwebui_instance_id" {
+  value = module.openwebui.instance_id
+}
